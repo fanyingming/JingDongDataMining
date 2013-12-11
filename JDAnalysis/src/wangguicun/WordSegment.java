282e80a2b2bc3b77a181c804cdcffc67af6aecc5
wangguicun <279660094@qq.com>
2013-12-11 14:58:19 +0800

    增加对抓取的数据进行分析的工程，包括对品牌数量统计，价格区间分布等； 增加.gitignore；

---
+++ JDAnalysis/src/wangguicun/WordSegment.java
@@ -0,0 +1,47 @@
+package wangguicun;
+import java.io.*;
+public class WordSegment 
+{
+	String dir_exe="";
+	String arg1="";
+	String arg2="";
+	
+	String strResult="#";
+	
+	public WordSegment()
+	{
+		dir_exe="C:\\Users\\guicun\\workspace\\wangguicun\\bin\\Segment\\devidewords.exe";
+		arg1=" C:\\Users\\guicun\\workspace\\wangguicun\\bin\\Segment\\pangu\\PanGu.xml ";
+		arg2="";
+	}
+	
+	@SuppressWarnings("finally")
+	public String ExecuteWordSegmentation()
+	{
+		try 
+		{ 
+			Process p;
+			String cmd=dir_exe+arg1+arg2;
+			p=java.lang.Runtime.getRuntime().exec(cmd); 
+		
+			BufferedInputStream br = new BufferedInputStream(p.getInputStream());
+			BufferedReader inBr = new BufferedReader(new InputStreamReader(br));  
+			String lineStr=null;  
+			lineStr = inBr.readLine();
+			inBr.close();
+			br.close();
+			
+			String []segresult=lineStr.split("-");
+			strResult=segresult[0];
+			if(segresult[0].length()<2) strResult=segresult[0]+segresult[1];
+			else if(segresult[0].length()==2&&segresult[1].length()<2) strResult=segresult[0]+segresult[1];
+		} catch (IOException e) 
+		{ 
+			System.out.println("·Ö´Ê³ÌÐò´íÎó");
+			e.printStackTrace(); 
+		} finally
+		{
+			return strResult;
+		}
+	}
+}
